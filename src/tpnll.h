f331f7a75ff0215a949d9593507df883eea88dfa
RedNeath <theandre.bourry@gmail.com>
2023-11-11 11:59:37 +0100

    [EVO] Import correction & global import header

---
+++ src/tpnll.h
@@ -0,0 +1,9 @@
+/**
+ * Global framework import header. Including it will automatically include all the functions the
+ * framework provides.
+ */
+
+#include "ll_constants.h"
+#include "chron.h"
+#include "logger.h"
+#include "string_manipulation.h"
